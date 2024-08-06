f21ec5d0bb64e6b6d8696131f01971965e10c667
HadrioLuiz <hadrio.luiz@gmail.com>
2024-08-06 12:18:35 -0300

    Corretion instaFeed posts

--- src/Components/InstaFeed.js
+++ src/Components/InstaFeed.js
@@ -37,21 +37,21 @@ const InstagramCarousel = () => {
     return (
         <div className='bg-slate-400 overflow-hidden'>
             <Slider {...settings}>
-                {posts.map(post => (
-                    <div key={post.id} className='text-center pb-16 pt-10'>
-                        {post.media_type === 'IMAGE' && (
+                {posts.map(posts => (
+                    <div key={posts.id} className='text-center pb-16 pt-10'>
+                        {posts.media_type === 'IMAGE' && (
                             <img
-                                src={post.media_url}
-                                alt={post.caption}
+                                src={posts.media_url}
+                                alt={posts.caption}
                                 className='ml-40 rounded-xl h-[550px] w-[1100px]' />
                         )}
-                        {post.media_type === 'VIDEO' && (
+                        {posts.media_type === 'VIDEO' && (
                             <video controls>
-                                <source src={post.media_url} type="video/mp4" />
+                                <source src={posts.media_url} type="video/mp4" />
                             </video>
                         )}
                         <div className='text-5xl mt-[-80px] text-[#f0f3df] p-0 text-left ml-52'>
-                            <h1 className={open_sans.className}>{post.caption}</h1>                   
+                            <h1 className={open_sans.className}>{posts.caption}</h1>                   
                         </div>
                     </div>
                 ))}
