d06efa150a2f52e0b4bdc25e68b0f6a4c3f6d37d
HadrioLuiz <hadrio.luiz@gmail.com>
2024-08-30 14:53:54 -0300

    Minor Update Menu Button

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -5,7 +5,7 @@ import { Open_Sans } from "next/font/google";
 import { usePathname } from "next/navigation";
 import { IoMenu } from "react-icons/io5";
 import { IoCloseSharp } from "react-icons/io5";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const open_sans = Open_Sans({
     weight: '600',
@@ -60,9 +60,7 @@ export default function Menu() {
                         type="button"
                         className="text-white focus:outline-none"
                     >
-                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}></path>
-                        </svg>
+                         {isOpen ? <IoCloseSharp size={24} /> : <IoMenu size={24} />} {/* Ícones de menu e fechar */}
                     </button>
                 </div>
             </div>
@@ -105,54 +103,3 @@ export default function Menu() {
         </nav>
     );
 }
-
-// export default function Menu() {
-
-
-//     return (
-//         <div className={open_sans.className}>
-//             <nav className="w-full h-10 bg-[#141759]">
-//                 <div className={getMenuClasses()}>
-//                     <ul className="flex justify-center text-white align-middle pt-2 items-center space-x-3">
-//                         <li className=" hover:underline">
-//                             
-//                         </li>
-//                         <li className=" hover:underline">
-//                             <Link className={`link ${usePathname === '/SoccerPage' ? 'active' : ''}`} href="/SoccerPage">
-//                                 Futebol
-//                             </Link>
-//                         </li>
-//                         <li className=" hover:underline">
-//                             <Link className={`link ${usePathname === '/StorePage' ? 'active' : ''}`} href="/StorePage">
-//                                 Loja
-//                             </Link>
-//                         </li>
-//                         <li className=" hover:underline">
-//                             <Link className={`link ${usePathname === '/TrophyPage' ? 'active' : ''}`} href="/TrophyPage">
-//                                 Troféus
-//                             </Link>
-//                         </li>
-//                         <li className=" hover:underline">
-//                             <Link className={`link ${usePathname === '/ContactPage' ? 'active' : ''}`} href="/ContactPage">
-//                                 Contato
-//                             </Link>
-//                         </li>
-//                         <li className=" hover:underline">
-//                             <Link className={`link ${usePathname === '/FanMemberPage' ? 'active' : ''}`} href="/FanMemberPage">
-//                                 Sócio Torcedor
-//                             </Link>
-//                         </li>
-//                     </ul>
-//                 </div>
-//                 <div className="md:hidden flex justify-end items-end mr-1 pt-1">
-//                     <button
-//                         onClick={() => {
-//                             setIsOpen(!isOpen);
-//                         }}>
-//                         <IoMenu color="white" className="w-8 h-8 justify-center" />
-//                     </button>
-//                 </div>
-//             </nav>
-//         </div>
-//     )
-// }
